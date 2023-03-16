81cb24c6ffb8684652598f5ba12b663cfd1213a7
Waylon <waylon@fireworkhq.com>
2023-03-16 15:52:43 +0800

    use autoservice

--- libraries/glide-image-loader/src/main/java/com/ld/glide_image_loader/GlideImageLoader.kt
+++ libraries/glide-image-loader/src/main/java/com/ld/glide_image_loader/GlideImageLoader.kt
@@ -2,8 +2,10 @@ package com.ld.glide_image_loader
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.google.auto.service.AutoService
 import com.ld.my_image_loader.MyImageLoader
 
+@AutoService(MyImageLoader::class)
 class GlideImageLoader: MyImageLoader {
 
     override fun load(imageView: ImageView, imageUrl: String) {
--- libraries/picasso-image-loader/src/main/java/com/ld/picasso_image_loader/PicassoImageLoader.kt
+++ libraries/picasso-image-loader/src/main/java/com/ld/picasso_image_loader/PicassoImageLoader.kt
@@ -1,9 +1,11 @@
 package com.ld.picasso_image_loader
 
 import android.widget.ImageView
+import com.google.auto.service.AutoService
 import com.ld.my_image_loader.MyImageLoader
 import com.squareup.picasso.Picasso
 
+@AutoService(MyImageLoader::class)
 class PicassoImageLoader: MyImageLoader {
 
     override fun load(imageView: ImageView, imageUrl: String) {
